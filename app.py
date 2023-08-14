5e5861682306cac5bc579257316636a2a3808287
rayala144 <rayalaakhil1999@gmail.com>
2023-08-14 11:50:13 +0530

    Formatted app.py

--- app.py
+++ app.py
@@ -9,14 +9,14 @@ app = Flask(__name__)
 # app.config['TEMPLATES_AUTO_RELOAD'] = True
 # app.config['TEMPLATE_FOLDER'] = os.path.abspath('templates')
 
-@app.route('/', methods=['GET', 'POST'])
 
+@app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
 
         if 'file' not in request.files:
             return "File not uploaded yet"
-        
+
         file = request.files['file']
 
         if file.filename.endswith('.xlsx') or file.filename.endswith('.xls'):
@@ -33,11 +33,12 @@ def upload_file():
 
             # Return the updated file for download
             return send_file(output, as_attachment=True, mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', download_name=output_file)
-        
+
         else:
             return 'Invalid file format. Only Excel files are allowed'
-        
+
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
