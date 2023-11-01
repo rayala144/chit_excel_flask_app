242fbe950bf924359e090b0733258d23757e3517
rakhil <rakhil@cisco.com>
2023-11-01 17:00:15 +0530

    Changed alert message

--- app.py
+++ app.py
@@ -15,29 +15,39 @@ def upload_file():
     if request.method == 'POST':
 
         file = request.files['file']
+        
+        if file.filename:
 
-        if file.filename.endswith('.xlsx') or file.filename.endswith('.xls'):
+            if file.filename.endswith('.xlsx') or file.filename.endswith('.xls'):
 
-            my_workbook = load_workbook(filename=BytesIO(file.read()))
-            updated_workbook = update_excel(my_workbook)
+                my_workbook = load_workbook(filename=BytesIO(file.read()))
+                updated_workbook = update_excel(my_workbook)
 
-            # output_file = add_suffix_to_filename(file, '_updated')
-            output = BytesIO()
-            updated_workbook.save(output)
-            output.seek(0)
+                # output_file = add_suffix_to_filename(file, '_updated')
+                output = BytesIO()
+                updated_workbook.save(output)
+                output.seek(0)
 
-            output_file = add_suffix_to_filename(file.filename, '_updated')
+                output_file = add_suffix_to_filename(file.filename, '_updated')
 
-            # Return the updated file for download
-            return send_file(output, as_attachment=True, mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', download_name=output_file)
+                # Return the updated file for download
+                return send_file(output, as_attachment=True, mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', download_name=output_file)
 
+            else:
+                return '''
+                    <script>
+                        alert("Only excel files are allowed");
+                        window.location = "/";
+                    </script>
+                '''
         else:
             return '''
-                <script>
-                    alert("File not uploaded yet");
-                    window.location = "/";
-                </script>
-            '''
+                    <script>
+                        alert("File not uploaded yet");
+                        window.location = "/";
+                    </script>
+                '''
+        
 
     return render_template('index.html')
 
