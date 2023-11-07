aa6906cf4c146e815aa0f6be68eb12dc9525e0cc
rakhil <rakhil@cisco.com>
2023-11-07 21:30:43 +0530

    Changed alert messages

--- app.py
+++ app.py
@@ -3,43 +3,53 @@ from io import BytesIO
 from openpyxl import load_workbook
 from mod_excel import *
 
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 # app.config['TEMPLATES_AUTO_RELOAD'] = True
 # app.config['TEMPLATE_FOLDER'] = os.path.abspath('templates')
+
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
 
-        if 'file' not in request.files:
-            return "File not uploaded yet"
-
         file = request.files['file']
 
-        if file.filename.endswith('.xlsx') or file.filename.endswith('.xls'):
+        if file.filename:
 
-            my_workbook = load_workbook(filename=BytesIO(file.read()))
-            updated_workbook = update_excel(my_workbook)
+            if file.filename.endswith('.xlsx') or file.filename.endswith('.xls'):
 
-            # output_file = add_suffix_to_filename(file, '_updated')
-            output = BytesIO()
-            updated_workbook.save(output)
-            output.seek(0)
+                my_workbook = load_workbook(filename=BytesIO(file.read()))
+                updated_workbook = update_excel(my_workbook)
 
-            output_file = add_suffix_to_filename(file.filename, '_updated')
+                # output_file = add_suffix_to_filename(file, '_updated')
+                output = BytesIO()
+                updated_workbook.save(output)
+                output.seek(0)
 
-            # Return the updated file for download
-            return send_file(output, as_attachment=True,
-                             mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-                             download_name=output_file)
+                output_file = add_suffix_to_filename(file.filename, '_updated')
 
+                # Return the updated file for download
+                return send_file(output, as_attachment=True, mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', download_name=output_file)
+
+            else:
+                return '''
+                    <script>
+                        alert("Only excel files are allowed");
+                        window.location = "/";
+                    </script>
+                '''
         else:
-            return 'Invalid file format. Only Excel files are allowed'
+            return '''
+                    <script>
+                        alert("File not uploaded yet");
+                        window.location = "/";
+                    </script>
+                '''
 
     return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
